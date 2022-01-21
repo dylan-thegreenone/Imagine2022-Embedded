a96cb64b7ca5eb7a354d5d05b4e9057c0098aff1
dylan-thegreenone <74738264+dylan-thegreenone@users.noreply.github.com>
2022-01-21 17:40:50 -0500

    Added include guard

--- components/udp/include/udp.h
+++ components/udp/include/udp.h
@@ -1,3 +1,6 @@
+#ifndef UDP
+#define UDP
+
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -6,3 +9,5 @@
 int send_str(int sock, struct sockaddr_in* destaddr, char* msg_str, int block_size);
 struct sockaddr_in* construct_sockaddr_info(struct sockaddr_in* destaddr, char* addr, int port, int af);
 int default_af();
+
+#endif
