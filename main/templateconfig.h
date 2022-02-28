e25534583973184bc83aa77342e1fb787c2c010c
Dylan Greenberg <dtg2565>
2022-02-28 14:04:12 -0500

    generalized template config file

--- main/templateconfig.h
+++ main/templateconfig.h
@@ -4,8 +4,8 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
-#define DESTINATION_ADDRESS "129.21.131.22"
-#define DESTINATION_PORT 6666
+#define DESTINATION_ADDRESS "server_ip_here"
+#define DESTINATION_PORT port_number_here
 
 // to disable wifi connection, comment out defining a password
 #define WIFI_SSID "ssid_here"
